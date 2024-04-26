732b5ae9b8bc5639d76ceab613846d6fb21d8241
Samrat Alam <samrat.alam@reddotdigitalit.com>
2024-04-26 14:27:04 +0600

    initial setup

---
+++ src/main/java/com/example/demo_ecommerce_app/DemoECommerceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_ecommerce_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoECommerceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoECommerceAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_ecommerce_app/DemoECommerceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_ecommerce_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoECommerceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
